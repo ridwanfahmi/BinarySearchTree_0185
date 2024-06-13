46e9ba8c6cf76b329c20f5371a9b9d0571c4f8c0
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-13 11:45:12 +0700

    membuat case 4

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -154,6 +154,11 @@ int main()
             x.preorder(x.ROOT);
             break;
         }
+        case '4':
+        {
+            x.postorder(x.ROOT);
+            break;
+        }
         
         }
     }
