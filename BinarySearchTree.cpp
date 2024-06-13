edd1536c26160285ecb9e7fc0f01a8954a0b0ea6
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-13 11:41:01 +0700

    membuat case 1

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -130,6 +130,20 @@ int main()
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) : ";
 
-        
+        char ch;
+        cin >> ch;
+        cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            cout << "Enter a word: ";
+            string word;
+            cin >> word;
+            x.insert(word);
+            break;
+        }
+        }
     }
 }
