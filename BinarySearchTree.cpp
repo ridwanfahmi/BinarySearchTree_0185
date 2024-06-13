2169421778529289b05360724aaecff46bff16cd
TOSHIBA <ridwanfahmidina12@gmail.com>
2024-06-13 11:15:10 +0700

    membuat method search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -52,6 +52,24 @@ public:
             parent->rightchild = newNode;  //make the right child of the parent point to new node
         }
     }
+
+    void search(string element, Node*& parent, Node*& currentNode)
+    {
+        //this function seacrhes the currentNode of the specified Node as well as the current node of its parent
+
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                currentNode = currentNode->rightchild;
+        }
+    }
+
+
 };
 
 
